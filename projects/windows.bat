747095b3d311b5b6e4eb7d94791f33b3c2a3b0ec
Embedthis Software <dev@embedthis.com>
2013-01-31 11:42:31 -0800

    Pass ARCH to windows.bat

--- projects/windows.bat
+++ projects/windows.bat
@@ -4,12 +4,12 @@ REM   Set VS vars and invoke nmake
 REM
 
 if DEFINED VSINSTALLDIR GOTO DONE
-    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" %1
 if DEFINED VSINSTALLDIR GOTO DONE
-    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %1
 if DEFINED VSINSTALLDIR GOTO DONE
-    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" %1
 :DONE
 
-echo nmake %1 %2 %3 %4 %5 %6 %7 %8 %9
-nmake %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo nmake %2 %3 %4 %5 %6 %7 %8 %9
+nmake %2 %3 %4 %5 %6 %7 %8 %9
