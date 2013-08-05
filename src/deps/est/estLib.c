bee0adad692ef4260b9abee922398f2ac06f02b8
Embedthis Software <dev@embedthis.com>
2013-08-05 14:27:16 -0700

    Fix MPR_INLINE for DIAB

--- src/deps/est/estLib.c
+++ src/deps/est/estLib.c
@@ -11407,8 +11407,9 @@ char *ssl_get_cipher(ssl_context * ssl)
  */
 PUBLIC int ssl_handshake(ssl_context * ssl)
 {
+#if BIT_EST_LOGGING
     char    cbuf[4096];
-
+#endif
     int old_state = ssl->state;
     int ret = EST_ERR_SSL_FEATURE_UNAVAILABLE;
 
@@ -11425,6 +11426,7 @@ PUBLIC int ssl_handshake(ssl_context * ssl)
 #endif
     SSL_DEBUG_MSG(2, ("<= handshake"));
     
+#if BIT_EST_LOGGING
     if (ssl->state == SSL_HANDSHAKE_OVER && old_state != SSL_HANDSHAKE_OVER) {
         SSL_DEBUG_MSG(1, ("using cipher: %s", ssl_get_cipher(ssl)));
         if (ssl->peer_cert) {
@@ -11433,6 +11435,7 @@ PUBLIC int ssl_handshake(ssl_context * ssl)
             SSL_DEBUG_MSG(1, ("Peer supplied no certificate"));
         }
     }
+#endif
     return ret;
 }
 
