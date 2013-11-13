0b055fbe0bf9e9be72741277084d3cb659dfe7d8
Embedthis Software <dev@embedthis.com>
2013-11-13 11:29:03 -0800

    Set _FILE_OFFSET_BITS conditionally based on __USE_FILE_OFFSET64.
    Fixes building on Linux 2.4 which lacks sendfile64.
    Message:
    
    /usr/include/sys/sendfile.h:26:3: #error "<sys/sendfile.h> cannot be used with _FILE_OFFSET_BITS=64"

--- src/bitos.h
+++ src/bitos.h
@@ -292,7 +292,9 @@
     #define _GNU_SOURCE 1
     #if !BIT_64
         #define _LARGEFILE64_SOURCE 1
-        #define _FILE_OFFSET_BITS 64
+        #ifdef __USE_FILE_OFFSET64
+            #define _FILE_OFFSET_BITS 64
+        #endif
     #endif
 #endif
 
