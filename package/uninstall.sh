737fa0e3741ae3d99063df292d89dd818e2a1fd5
Embedthis Software <dev@embedthis.com>
2014-04-18 18:06:13 -0700

    CLEAN: uninstall.sh

--- package/uninstall.sh
+++ package/uninstall.sh
@@ -7,26 +7,6 @@
 #	Usage: uninstall [configFile]
 #
 ################################################################################
-#
-#	Copyright (c) Embedthis Software LLC, 2003-2014. All Rights Reserved.
-#	The latest version of this code is available at http://embedthis.com
-#
-#	This software is open source; you can redistribute it and/or modify it 
-#	under the terms of the GNU General Public License as published by the 
-#	Free Software Foundation; either version 2 of the License, or (at your 
-#	option) any later version.
-#
-#	This program is distributed WITHOUT ANY WARRANTY; without even the 
-#	implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-#	See the GNU General Public License for more details at:
-#	http://embedthis.com/downloads/gplLicense.html
-#	
-#	This General Public License does NOT permit incorporating this software 
-#	into proprietary programs. If you are unable to comply with the GPL, a 
-#	commercial license for this software and support services are available
-#	from Embedthis Software at http://embedthis.com
-#
-################################################################################
 
 HOME=`pwd`
 FMT=
