599e6bf03b73c8ece31744c5f0b10de6274bc13c
Embedthis Software <dev@embedthis.com>
2014-07-30 10:21:03 -0700

    REFACTOR: version 3.3.5

--- projects/goahead-windows-default-me.h
+++ projects/goahead-windows-default-me.h
@@ -208,7 +208,7 @@
     #define ME_TITLE "Embedthis GoAhead"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "3.3.4"
+    #define ME_VERSION "3.3.5"
 #endif
 
 /* Prefixes */
@@ -306,10 +306,10 @@
     #define ME_MINOR_VERSION 3
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 4
+    #define ME_PATCH_VERSION 5
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 300030004
+    #define ME_VNUM 300030005
 #endif
 
 /* Components */
