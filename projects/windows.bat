451f362205367b6ef806022ffecf73f744657cd8
Embedthis Software <dev@embedthis.com>
2014-10-15 07:52:36 -0700

    FIX: windows.bat for VS 2013

--- projects/windows.bat
+++ projects/windows.bat
@@ -1,13 +1,22 @@
 @echo off
 REM
-REM   Set VS vars and invoke nmake   
+REM   Set VS vars and run a command
 REM
 
+SET PATH=\perl64\bin;\perl\bin;%PATH%
+
+set VS=
 if DEFINED VSINSTALLDIR GOTO DONE
-for /l %%v in (9, 1, 14) do (
+for /l %%v in (14, -1, 9) do (
+    set VS=%%v
     IF EXIST "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" call "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" %1
+    IF EXIST "%PROGRAMFILES(x86)%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" goto :done
     IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" call "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" %1
+    IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio %%v.0\VC\vcvarsall.bat" goto :done
 )
+VS=
 :DONE
 
-nmake -C %2 %3 %4 %5 %6 %7 %8 %9
+echo Using Visual Studio %VS%.0
+echo %2 %3 %4 %5 %6 %7 %8 %9
+%2 %3 %4 %5 %6 %7 %8 %9
