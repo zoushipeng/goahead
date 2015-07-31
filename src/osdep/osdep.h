452d49ff11e52bb960e9ea4b9b01fe51effca760
Embedthis Software <dev@embedthis.com>
2015-07-31 10:52:59 -0700

    FIX: bool definition for VS 2015

--- src/osdep/osdep.h
+++ src/osdep/osdep.h
@@ -280,6 +280,8 @@
     #endif
     /* 
         Work-around to allow the windows 7.* SDK to be used with VS 2012 
+        MSC_VER 1800 2013
+        MSC_VER 1900 2015
      */
     #if _MSC_VER >= 1700
         #define SAL_SUPP_H
@@ -485,7 +487,9 @@
             /**
                 Boolean data type.
              */
-            typedef char bool;
+            #if _MSC_VER <= 1800
+                typedef char bool;
+            #endif
         #endif
     #endif
 #endif
