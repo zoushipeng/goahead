0fae7b3f5dba3c96a000c8aeb048707c03f2c46e
Embedthis Software <dev@embedthis.com>
2015-08-21 20:39:58 -0700

    DEV: add configure.bat

---
+++ configure.bat
@@ -0,0 +1,8 @@
+@echo off
+REM
+REM   configure -- Configure for building
+REM
+REM   Configure will invoke "me configure" to prepare for building.
+REM   If building via "nmake" configuration is not required.
+REM
+me configure %1 %2 %3 %4 %5 %6 %7 %8 %9
