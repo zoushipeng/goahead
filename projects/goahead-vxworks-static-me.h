99a4541eb0d0d35171458144370233cd769424ab
Embedthis Software <dev@embedthis.com>
2015-11-12 16:25:10 -0800

    DEV: bump version

--- projects/goahead-vxworks-static-me.h
+++ projects/goahead-vxworks-static-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "3.5"
+    #define ME_COMPATIBLE "3.6"
 #endif
 #ifndef ME_COMPILER_HAS_ATOMIC
     #define ME_COMPILER_HAS_ATOMIC 0
@@ -256,7 +256,7 @@
     #define ME_TITLE "Embedthis GoAhead"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "3.5.1"
+    #define ME_VERSION "3.6.0"
 #endif
 
 /* Prefixes */
@@ -309,7 +309,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/goahead-3.5.1"
+    #define ME_SRC_PREFIX "/usr/src/goahead-3.6.0"
 #endif
 
 /* Suffixes */
@@ -348,13 +348,13 @@
     #define ME_MAJOR_VERSION 3
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 5
+    #define ME_MINOR_VERSION 6
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 1
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 300050001
+    #define ME_VNUM 300060000
 #endif
 
 /* Components */
