6ff4fb0eeb78bdf36de3f4a0da67a797bd2db6d0
Embedthis Software <dev@embedthis.com>
2015-11-27 09:38:59 -0800

    FIX: rename rom-documents

---
+++ src/rom.c
@@ -0,0 +1,16 @@
+/*
+   rom-documents.c
+   Compiled by webcomp: Mon Jan 14 14:13:54 2013
+ */
+
+#include "goahead.h"
+
+#if ME_ROM
+WebsRomIndex websRomIndex[] = {
+	{ 0, 0, 0 }
+};
+#else
+WebsRomIndex websRomIndex[] = {
+	{ 0, 0, 0 }
+};
+#endif
