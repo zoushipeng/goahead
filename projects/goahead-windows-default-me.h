158c597aafc3d8513dc8b221c5fae0952ac46a0b
Michael O'Brien <mob@emobrien.com>
2017-06-16 08:03:48 -0700

    DEV: prepare version 4

--- projects/goahead-windows-default-me.h
+++ projects/goahead-windows-default-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "3.6"
+    #define ME_COMPATIBLE "4.0"
 #endif
 #ifndef ME_COMPILER_FORTIFY
     #define ME_COMPILER_FORTIFY 1
@@ -247,7 +247,7 @@
     #define ME_TITLE "Embedthis GoAhead"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "3.6.6"
+    #define ME_VERSION "4.0.0"
 #endif
 
 /* Prefixes */
@@ -339,16 +339,16 @@
 
 /* Miscellaneous */
 #ifndef ME_MAJOR_VERSION
-    #define ME_MAJOR_VERSION 3
+    #define ME_MAJOR_VERSION 4
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 6
+    #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 6
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 300060006
+    #define ME_VNUM 400000000
 #endif
 
 /* Components */
