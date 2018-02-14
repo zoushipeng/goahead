103eeecc3ce8c9f96193eeb2e6385deb61c2fff1
Michael O'Brien <mob@emobrien.com>
2018-02-15 07:25:18 +1100

    REGEN

--- projects/goahead-vxworks-static.mk
+++ projects/goahead-vxworks-static.mk
@@ -3,7 +3,7 @@
 #
 
 NAME                  := goahead
-VERSION               := 4.0.1
+VERSION               := 4.0.2
 PROFILE               ?= static
 ARCH                  ?= $(shell echo $(WIND_HOST_TYPE) | sed 's/-.*$(ME_ROOT_PREFIX)/')
 CPU                   ?= $(subst X86,PENTIUM,$(shell echo $(ARCH) | tr a-z A-Z))
