ab9a289768e273b90d91347d2334fdf6cfcc89fa
Michael O'Brien <dev@embedthis.com>
2018-12-03 17:49:11 +1100

    FIX: windows VS 2017 building

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-projects\windows x86 nmake -f projects/goahead-windows-default.nmake %1 %2 %3 %4 %5 %6 %7 %8 %9
+projects\windows AMD64 nmake -f projects/appweb-windows-default.nmake %1 %2 %3 %4 %5 %6 %7 %8 %9
