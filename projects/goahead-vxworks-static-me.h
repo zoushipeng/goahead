37923b11046b115a4b7c24ca382563d3e69a7e55
Michael O'Brien <dev@embedthis.com>
2018-12-19 08:55:17 +1100

    DEV: add projects/README

--- projects/goahead-vxworks-static-me.h
+++ projects/goahead-vxworks-static-me.h
@@ -271,7 +271,7 @@
     #define ME_TITLE "Embedthis GoAhead"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "4.1.0"
+    #define ME_VERSION "4.1.1"
 #endif
 
 /* Prefixes */
@@ -324,7 +324,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/goahead-4.1.0"
+    #define ME_SRC_PREFIX "/usr/src/goahead-4.1.1"
 #endif
 
 /* Suffixes */
@@ -366,10 +366,10 @@
     #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 400010000
+    #define ME_VNUM 400010001
 #endif
 
 /* Components */
