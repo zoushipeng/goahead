5b31032005913a598deed921e255dc62bf62ad6f
Michael O'Brien <dev@embedthis.com>
2018-12-24 09:09:40 +1100

    DEV: bump version

--- projects/goahead-vxworks-default-me.h
+++ projects/goahead-vxworks-default-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "4.1"
+    #define ME_COMPATIBLE "5.0"
 #endif
 #ifndef ME_COMPILER_FORTIFY
     #define ME_COMPILER_FORTIFY 1
@@ -268,7 +268,7 @@
     #define ME_TITLE "Embedthis GoAhead"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "4.1.1"
+    #define ME_VERSION "5.0.0"
 #endif
 
 /* Prefixes */
@@ -321,7 +321,7 @@
     #define ME_VAPP_PREFIX "deploy"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "/usr/src/goahead-4.1.1"
+    #define ME_SRC_PREFIX "/usr/src/goahead-5.0.0"
 #endif
 
 /* Suffixes */
@@ -357,16 +357,16 @@
 
 /* Miscellaneous */
 #ifndef ME_MAJOR_VERSION
-    #define ME_MAJOR_VERSION 4
+    #define ME_MAJOR_VERSION 5
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 1
+    #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 1
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 400010001
+    #define ME_VNUM 500000000
 #endif
 
 /* Components */
