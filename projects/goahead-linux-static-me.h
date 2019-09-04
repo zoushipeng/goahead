f5cdacd56edb208a4de66c518a78bee514da556f
Michael O'Brien <dev@embedthis.com>
2019-09-04 17:51:40 +1000

    DOC: regen projects

--- projects/goahead-linux-static-me.h
+++ projects/goahead-linux-static-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "5.0"
+    #define ME_COMPATIBLE "5.1"
 #endif
 #ifndef ME_COMPILER_FORTIFY
     #define ME_COMPILER_FORTIFY 1
@@ -277,7 +277,7 @@
     #define ME_TITLE "Embedthis GoAhead Community Edition"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "5.0.1"
+    #define ME_VERSION "5.1.0"
 #endif
 
 /* Prefixes */
@@ -297,7 +297,7 @@
     #define ME_APP_PREFIX "/usr/local/lib/goahead"
 #endif
 #ifndef ME_VAPP_PREFIX
-    #define ME_VAPP_PREFIX "/usr/local/lib/goahead/5.0.1"
+    #define ME_VAPP_PREFIX "/usr/local/lib/goahead/5.1.0"
 #endif
 #ifndef ME_BIN_PREFIX
     #define ME_BIN_PREFIX "/usr/local/bin"
@@ -330,7 +330,7 @@
     #define ME_CACHE_PREFIX "/var/spool/goahead/cache"
 #endif
 #ifndef ME_SRC_PREFIX
-    #define ME_SRC_PREFIX "goahead-5.0.1"
+    #define ME_SRC_PREFIX "goahead-5.1.0"
 #endif
 
 /* Suffixes */
@@ -369,13 +369,13 @@
     #define ME_MAJOR_VERSION 5
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 0
+    #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 1
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 500000001
+    #define ME_VNUM 500010000
 #endif
 
 /* Components */
--- projects/goahead-windows-static-me.h
+++ projects/goahead-windows-static-me.h
@@ -22,7 +22,7 @@
     #define ME_COMPANY "embedthis"
 #endif
 #ifndef ME_COMPATIBLE
-    #define ME_COMPATIBLE "5.0"
+    #define ME_COMPATIBLE "5.1"
 #endif
 #ifndef ME_COMPILER_FORTIFY
     #define ME_COMPILER_FORTIFY 1
@@ -265,7 +265,7 @@
     #define ME_TITLE "Embedthis GoAhead Community Edition"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "5.0.1"
+    #define ME_VERSION "5.1.0"
 #endif
 
 /* Prefixes */
@@ -360,13 +360,13 @@
     #define ME_MAJOR_VERSION 5
 #endif
 #ifndef ME_MINOR_VERSION
-    #define ME_MINOR_VERSION 0
+    #define ME_MINOR_VERSION 1
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 1
+    #define ME_PATCH_VERSION 0
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 500000001
+    #define ME_VNUM 500010000
 #endif
 
 /* Components */
