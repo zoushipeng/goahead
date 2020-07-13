3aa1b909140d810644d6ed7ac5f9a9307f80fcf4
Michael O'Brien <mob@emobrien.com>
2020-07-13 10:05:57 +1000

    FIX: upload file cleanup

--- src/upload.c
+++ src/upload.c
@@ -370,6 +370,7 @@ static bool processContentData(Webs *wp)
             }
             hashEnter(wp->files, wp->uploadVar, valueSymbol(file), 0);
             defineUploadVars(wp);
+            wp->currentFile = 0;
 
         } else if (wp->uploadVar) {
             /*
@@ -427,7 +428,6 @@ static char *getBoundary(Webs *wp, char *buf, ssize bufLen)
     }
     return 0;
 }
-
 
 
 WebsUpload *websLookupUpload(Webs *wp, cchar *key)
